459c3e05b23d2aa96f5b6a2622b0372b93a55d87
owner <rgstsvc@gmail.com>
2020-02-13 15:42:24 +0900

    [mod] スタイルを微調整

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,20 @@
 <template>
-  <header id="#top"></header>
+  <header id="#top">
+    neize
+  </header>
 </template>
 
 <script></script>
 
 <style lang="scss" scoped>
 header {
+  color: #ffffff;
+  background-color: #212a37;
+  font-size: 2rem;
+  font-weight: bold;
+  height: 3rem;
   width: 100%;
   margin: 0 auto;
+  text-align: center;
 }
 </style>
