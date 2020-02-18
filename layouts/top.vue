39be35593a1444d15f0a48a8efab9a98f3be332f
owner <rgstsvc@gmail.com>
2020-02-18 16:29:37 +0900

    [fix] トップページの幅がスマホ表示で狭くなる問題を修正

--- layouts/top.vue
+++ layouts/top.vue
@@ -15,4 +15,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 14px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
