1ade3079746571b123261b28d1b6bd27871dfb0e
owner <rgstsvc@gmail.com>
2020-03-14 11:53:13 +0900

    [mod] エラー文の設定

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,7 +2,7 @@
   <div class="error">
     <h1 class="error-title">{{error.statusCode}}</h1>
     <p class="error-txt" v-if="error.statusCode === 404">ページが見つかりません</p>
-    <p class="error-txt" v-else>エラーが発生しました</p>
+    <p class="error-txt" v-else>エラーが発生しました。しばらく経ってからもう一度お試しください。</p>
     <nuxt-link class="error-link" to="/">ホームに戻る</nuxt-link>
   </div>
 </template>
