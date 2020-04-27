b7de906b5a20f1ebe14e21ad2b5770a71d46205e
owner <rgstsvc@gmail.com>
2020-04-27 14:02:33 +0900

    Revert "test commit"
    This reverts commit b496817b938a2d0d03f15f605a5a731d8e3bb759.

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://nurse-line.igz0.net',
+    baseURL: 'https://wp.nurse-line.net',
   },
   router: {
     base: '/',
